722af49852950fc620b0280e0c1f3661001463e0
unknown <mansi@spottabl.com>
2024-04-30 09:06:06 +0530

    pred

--- predictor.py
+++ predictor.py
@@ -31,7 +31,7 @@ def app():
         Department = st.selectbox("What's the Department of Employee? ", ['Sales', 'Research & Development', 'Human Resources'])
         JobRole = st.selectbox("What's the Job Role of Employee? ", ['Sales Executive', 'Research Scientist', 'Laboratory Technician', 'Manufacturing Director', 'Healthcare Representative', 'Manager', 'Sales Representative', 'Research Director', 'Human Resources'])
         JobInvolvement = st.selectbox("Rate the Involvement of Employee? ", ['1. Low', '2. Medium', '3. High', '4. Very High'])
-        JobStatisfaction = st.selectbox("Choose the level of Job statisfaction of Employee ?", ['1. Low', '2. Medium', '3. High', '4. Very High'])
+        JobSatisfaction = st.selectbox("Choose the level of Job statisfaction of Employee ?", ['1. Low', '2. Medium', '3. High', '4. Very High'])
         DistanceFromHome = st.number_input("What's the Distance from Home to Office of Employee in kms? ")
         OverTime = st.selectbox("Does Employee works Over Time? ", ['Yes', 'No'])
         BusinessTravel = st.selectbox("What's the Business Travel frequency of Employee? ", ['Frequently', 'Rarely', 'No Travel'])
@@ -50,7 +50,7 @@ def app():
                 "DistanceFromHome": [DistanceFromHome],
                 "Education": [Education],
                 "JobInvolvement": [JobInvolvement],
-                "JobStatisfaction": [JobStatisfaction],
+                "JobSatisfaction": [JobSatisfaction],
                 "NumCompaniesWorked": [NumCompaniesWorked],
                 "OverTime": [OverTime],
                 "PercentSalaryHike": [PercentSalaryHike],
@@ -66,34 +66,33 @@ def app():
                 "MaritalStatus_Single": [MaritalStatus]
             })       
         
-        # Feature Engineering
-        data['Gender_Male'] = 1 if data['Gender_Male'].iloc[0] == 'Male' else 0
-        data['Education'] = 1 if data['Education'].iloc[0] == '1. Below College' else 2 if data['Education'].iloc[0] == '2. College' else 3 if data['Education'].iloc[0] == '3. Bachelor' else 4 if data['Education'].iloc[0] == '4. Master' else 5 if data['Education'].iloc[0] == '5. Doctor' else 1
-        data['MaritalStatus_Single'] = 1 if data['MaritalStatus_Single'].iloc[0] == 'Single' else 0
-        data['Department_Sales'] = 1 if data['Department_Sales'].iloc[0] == 'Sales' else 0
-        data['JobRole_Sales Representative'] = 1 if data['JobRole_Sales Representative'].iloc[0] == 'Sales Representative' else 0
-        data['JobInvolvement'] = 1 if data['JobInvolvement'].iloc[0] == '1. Low' else 2 if data['JobInvolvement'].iloc[0] == '2. Medium' else 3 if data['JobInvolvement'].iloc[0] == '3. High' else 4 if data['JobInvolvement'].iloc[0] == '4. Very High' else 1
-        data['JobStatisfaction'] = 1 if data['JobStatisfaction'].iloc[0] == '1. Low' else 2 if data['JobStatisfaction'].iloc[0] == '2. Medium' else 3 if data['JobStatisfaction'].iloc[0] == '3. High' else 4 if data['JobStatisfaction'].iloc[0] == '4. Very High' else 1
-        data['OverTime'] = 1 if data['OverTime'].iloc[0] == 'Yes' else 0
-        data['BusinessTravel_Travel_Frequently'] = 1 if data['BusinessTravel_Travel_Frequently'].iloc[0] == 'Frequently' else 0
-        data['WorkLifeBalance'] = 1 if data['WorkLifeBalance'].iloc[0] == '1. Low' else 2 if data['WorkLifeBalance'].iloc[0] == '2. Medium' else 3 if data['WorkLifeBalance'].iloc[0] == '3. High' else 4 if data['WorkLifeBalance'].iloc[0] == '4. Very High' else 1
-        data['RelationshipSatisfaction'] = 1 if data['WorkLifeBalance'].iloc[0] == '1. Low' else 2 if data['RelationshipSatisfaction'].iloc[0] == '2. Good' else 3 if data['RelationshipSatisfaction'].iloc[0] == '3. Excellent' else 4 if data['RelationshipSatisfaction'].iloc[0] == '4. Outstanding' else 1
+            # Feature Engineering 
+            data['Education'] = 1 if data['Education'].iloc[0] == '1. Below College' else 2 if data['Education'].iloc[0] == '2. College' else 3 if data['Education'].iloc[0] == '3. Bachelor' else 4 if data['Education'].iloc[0] == '4. Master' else 5 if data['Education'].iloc[0] == '5. Doctor' else 1
+            data['MaritalStatus_Single'] = 1 if data['MaritalStatus_Single'].iloc[0] == 'Single' else 0
+            data['Department_Sales'] = 1 if data['Department_Sales'].iloc[0] == 'Sales' else 0
+            data['JobRole_Sales Representative'] = 1 if data['JobRole_Sales Representative'].iloc[0] == 'Sales Representative' else 0
+            data['JobInvolvement'] = 1 if data['JobInvolvement'].iloc[0] == '1. Low' else 2 if data['JobInvolvement'].iloc[0] == '2. Medium' else 3 if data['JobInvolvement'].iloc[0] == '3. High' else 4 if data['JobInvolvement'].iloc[0] == '4. Very High' else 1
+            data['JobSatisfaction'] = 1 if data['JobSatisfaction'].iloc[0] == '1. Low' else 2 if data['JobSatisfaction'].iloc[0] == '2. Medium' else 3 if data['JobSatisfaction'].iloc[0] == '3. High' else 4 if data['JobSatisfaction'].iloc[0] == '4. Very High' else 1
+            data['OverTime'] = 1 if data['OverTime'].iloc[0] == 'Yes' else 0
+            data['BusinessTravel_Travel_Frequently'] = 1 if data['BusinessTravel_Travel_Frequently'].iloc[0] == 'Frequently' else 0
+            data['WorkLifeBalance'] = 1 if data['WorkLifeBalance'].iloc[0] == '1. Low' else 2 if data['WorkLifeBalance'].iloc[0] == '2. Medium' else 3 if data['WorkLifeBalance'].iloc[0] == '3. High' else 4 if data['WorkLifeBalance'].iloc[0] == '4. Very High' else 1
+            data['RelationshipSatisfaction'] = 1 if data['WorkLifeBalance'].iloc[0] == '1. Low' else 2 if data['RelationshipSatisfaction'].iloc[0] == '2. Good' else 3 if data['RelationshipSatisfaction'].iloc[0] == '3. Excellent' else 4 if data['RelationshipSatisfaction'].iloc[0] == '4. Outstanding' else 1
+            data['Gender_Male'] = 1 if data['Gender_Male'].iloc[0] == 'Male' else 0
+            # Scale the numerical columns
+            data = scaler_dict.transform(data)
+            # st.write(data)
 
-        # Scale the numerical columns
-        data = scaler_dict.transform(data)
-        st.write(data)
+            # Make prediction using the model
+            predictions = ml_components_dict.predict(data)
+            # st.write(predictions)
+            ans = predictions[0]
+            # Display the predictions
+            st.balloons()
 
-        # Make prediction using the model
-        predictions = ml_components_dict.predict(data)
-        # st.write(predictions)
-        ans = predictions[0]
-        # Display the predictions
-        st.balloons()
-
-        if ans>churn_cutoff:
-            st.success(f"The Employee is likely to leave the Company.")
-        else:
-            st.success(f"The Employee is happy to be a part of Company.")
+            if ans>churn_cutoff:
+                st.success(f"The Employee is likely to leave the Company.")
+            else:
+                st.success(f"The Employee is happy to be a part of Company.")
         
         # Display the predictions with custom styling
         # st.success(f"Predicted Churn Rate: {predictions[0]:,.2f}",icon="✅")
